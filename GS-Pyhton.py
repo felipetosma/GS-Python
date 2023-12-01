ce00e33e00b79f3eb458bfd346a2ddae189fcd78
Pedro Araujo <91104140+Araujopedro@users.noreply.github.com>
2023-12-01 18:45:30 -0300

    Adição de pessoas de confiança e função "exibir usuário"

--- GS-Pyhton.py
+++ GS-Pyhton.py
@@ -18,4 +18,36 @@ def cadastrar_usuario(lista_usuarios):
         resp = int(input("Mais alguma doença? (1) - Não(0): "))
         doencas_em_tratamento.append(doencas_tratamento)
 
+    # Adicionar pessoas de confiança ou não:
+    resp = 2
+    contatos_de_confianca = []
+    while (resp == 2):
+        nome_pessoa_confianca = input("Digite o nome da pessoa de confiança: ")
+        tel_pessoa_confianca = input("Digite o telefone da pessoa de confiança: ")
+        resp = int(input("Adicionar mais uma pessoa de confiança? (2) - Finalizar cadastro (0): "))
+        contatos_de_confianca.append({"Nome": nome_pessoa_confianca, "Telefone": tel_pessoa_confianca})
+
+    novo_usuario = {
+        "Nome": nome,
+        "Data de Nascimento": data_nascimento,
+        "Tipo Sanguíneo": tipo_sanguineo,
+        "Telefone": telefone,
+        "Doenças em Tratamento": doencas_em_tratamento,
+        "Contatos de Confiança": contatos_de_confianca,
+    }
+
+    lista_usuarios.append(novo_usuario)
+    print("Usuário cadastrado com sucesso!")
+
+def exibir_usuarios(lista_usuarios):
+    print(("_______________________"))
+    print("\nLista de Usuários:")
+    for usuario in lista_usuarios:
+        print(f"\nNome: {usuario['Nome']}")
+        print(f"Data de Nascimento: {usuario['Data de Nascimento']}")
+        print(f"Tipo Sanguíneo: {usuario['Tipo Sanguíneo']}")
+        print(f"Telefone: {usuario['Telefone']}")
+        print(f"Doenças em Tratamento: {', '.join(usuario['Doenças em Tratamento'])}")
+        for contato in usuario['Contatos de Confiança']:
+            print(f"Pessoa de Confiança: {contato['Nome']}, Telefone: {contato['Telefone']}")
 
