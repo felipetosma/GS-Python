277e0f325f04f4a0f51c8a3d166099507e49cef8
Felipe Amador <felipe.amadoor@gmail.com>
2023-11-30 22:29:23 -0300

    Commit ajuste cadastro usuário

--- GS-Pyhton.py
+++ GS-Pyhton.py
@@ -1,7 +1,14 @@
 def cadastrar_usuario(lista_usuarios):
     nome = input("Digite o nome do usuário: ")
-    data_nascimento = input("Digite a data de nascimento do usuário (DD/MM/AAAA): ")
+
+    # Validar data de nascimento:
+    data_nascimento = []
+    while (len(data_nascimento) != 10):
+        data_nascimento = input("Digite a data de nascimento do usuário (DD/MM/AAAA): ")
+        if (len(data_nascimento) != 10 or data_nascimento[2] != '/' or data_nascimento[5] != '/'):
+            print("Formato de data inválido. Tente novamente.")
+    tipo_sanguineo = []
+    while(tipo_sanguineo )
     tipo_sanguineo = input("Digite o tipo sanguíneo do usuário: ")
     telefone = input("Digite o telefone do usuário: ")
-    doencas_tratamento = input("Digite as doenças em tratamento do usuário (separadas por vírgula): ").split(',')
-    doencas_cronicas = input("Digite as doenças crônicas do usuário (separadas por vírgula): ").split(',')
+
